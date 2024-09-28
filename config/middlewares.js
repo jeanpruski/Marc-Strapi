dd4ad1db8fac4d07d7e9a8aaa172269e8e439dfb
Prjski <jean.pruski@gmail.com>
2024-09-28 11:56:24 +0200

    change cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,8 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = ({ env }) => ({
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['http://localhost:3000', 'https://marc-strapi.onrender.com'],
+    },
+  },
+});
